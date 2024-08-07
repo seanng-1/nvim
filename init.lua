35e5db8a003f82a93e6638826ba4b72e7b4b5f82
Sean Geles <seang_173@hotmail.com>
2024-08-08 08:33:15 +1000

    Better git diff

--- init.lua
+++ init.lua
@@ -246,13 +246,15 @@ require('lazy').setup({
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
+      current_line_blame = true,
+      numhl = true,
+      -- signs = {
+      --   add = { text = '+' },
+      --   change = { text = '~' },
+      --   delete = { text = '_' },
+      --   topdelete = { text = '‾' },
+      --   changedelete = { text = '~' },
+      -- },
     },
   },
 
